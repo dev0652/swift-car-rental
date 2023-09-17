e27a3b13c48fc8a3528d9e853009bd786df542ca
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-09-17 22:51:49 +0300

    attempt to fix build to gh pages

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,27 @@ import react from '@vitejs/plugin-react';
 
 // https://medium.com/@pushplaybang/absolutely-dont-use-relative-paths-imports-in-your-vite-react-project-c8593f93bbea
 
-export default defineConfig({
-  base: '/',
-  resolve: {
-    alias: {
-      src: '/src',
-      components: '/src/components',
-      pages: '/src/pages',
-      services: '/src/services',
-      styles: '/src/styles',
-      images: '/src/images',
-      assets: '/src/assets',
-      data: '/src/data',
+export default defineConfig(({ command }) => {
+  const config = {
+    base: '/',
+    resolve: {
+      alias: {
+        src: '/src',
+        components: '/src/components',
+        pages: '/src/pages',
+        services: '/src/services',
+        styles: '/src/styles',
+        images: '/src/images',
+        assets: '/src/assets',
+        data: '/src/data',
+      },
     },
-  },
-  plugins: [react()],
+    plugins: [react()],
+  };
+
+  if (command !== 'serve') {
+    config.base = '/swift-car-rental';
+  }
+
+  return config;
 });
