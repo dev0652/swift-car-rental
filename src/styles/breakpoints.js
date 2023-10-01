474846d1885cf32218c9f90ea0c68aecdea02439
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-10-01 22:15:16 +0300

    changed breakpoints

--- src/styles/breakpoints.js
+++ src/styles/breakpoints.js
@@ -1,5 +1,7 @@
 export const breakpoints = {
   mobile: '375px',
-  tablet: '768px',
-  desktop: '1280px',
+  // tablet: '768px',
+  // desktop: '1280px',
+  tablet: '600px', // for 8 inch
+  desktop: '960px', // for older screens
 };
